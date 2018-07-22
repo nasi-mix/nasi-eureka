1ca2a81c62a5d36bdaae4dfaab12e359fc0867c1
qfdk <qfdk2010@gmail.com>
2018-07-22 20:02:37 +0200

    [Dev] first commit

---
+++ src/main/java/me/qfdk/nasimie/NasiEurekaApplication.java
@@ -0,0 +1,14 @@
+package me.qfdk.nasimie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class NasiEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasiEurekaApplication.class, args);
+	}
+}
